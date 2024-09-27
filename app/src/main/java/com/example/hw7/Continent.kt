2f0e4793a267c9f8da914d7d1f60c132c1261e99
kaasym1 <kasymmuhammaduulu@gmail.com>
2024-09-27 17:50:19 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw7/Continent.kt
@@ -0,0 +1,8 @@
+package com.example.hw7
+
+import java.io.Serializable
+
+data class Continent(
+    val image: String,
+    val title: String
+) : Serializable
---
+++ app/src/main/java/com/example/hw7/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.hw7
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().add(R.id.continents_rv, ContinentsFragment()).commit()
+    }
+}
